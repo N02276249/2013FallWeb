562d3cc5a8e97454dfa58669076083b5d0a3cf7e
unknown <N02276249@.active.newpaltz.edu>
2013-08-29 16:18:16 -0400

    Twitter Bootstrap

--- FINAL/index.php
+++ FINAL/index.php
@@ -1,3 +1,22 @@
-Hello this is the Final Project
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-Which is going to be an E-commerce Site
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../../assets/js/html5shiv.js"></script>
+      <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <h1>This is the Final Project.</h1>
+    <h1>It will be an E-Commerce website.</h1>
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
